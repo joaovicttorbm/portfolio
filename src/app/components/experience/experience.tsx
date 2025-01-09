21b9debde5ea5f799bc58b868b687e0b806f5de1
João Matos <117475547+joaovicttorbm@users.noreply.github.com>
2025-01-09 13:56:05 -0300

    Update experience.tsx

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -10,9 +10,9 @@ export function Experience() {
       <SectionTitle text="Experience" />
       <p>Software Developer, freelancer.</p>
       <div className="experience-time">
-        <Skill image="/react.png" measure={2} years="2 years" />
-        <Skill image="/ts.png" measure={2} years="2 years" />
-        <Skill image="/js.png" measure={2} years="2 years" />
+        <Skill image="/react.png" measure={1} years="1 years" />
+        <Skill image="/ts.png" measure={1} years="1 years" />
+        <Skill image="/js.png" measure={1} years="1 years" />
         <Skill image="/java.png" measure={1} years="1 year" />
       </div>
     </div>
